1f79fa88902e25406fa5036943941ff7547f7926
David Fischer <davidfischer99@gmx.de>
2022-01-22 14:25:07 +0100

    add banner and support for single end reads

--- scripts/create_input_csv.py
+++ scripts/create_input_csv.py
@@ -9,16 +9,18 @@ from collections import defaultdict
 
 def main(args=None):
 
-    parser = argparse.ArgumentParser(description="Description of your program", epilog="Example usage: python3 create_input_csv.py <FASTQ_DIR>" )
-    parser.add_argument("FASTQ_DIR", help="Description for foo argument")
+    parser = argparse.ArgumentParser(description="Description of your program", epilog="Example usage: python3 create_input_csv.py <FASTQ_DIR> <READ_MODE>" )
+    parser.add_argument("FASTQ_DIR", help="Directory containing the fastq.gz files.")
+    parser.add_argument("READ_MODE", help="Mode of the reads. Ommitting this can cause wrong sample names. [single, paired]")
     args = parser.parse_args()
 
-    fastq_dir = args.FASTQ_DIR
+    fastq_dir   = args.FASTQ_DIR
+    mode        = args.READ_MODE
     dictionary = defaultdict(list)
     WD = os.getcwd()
 
     files = sorted(glob.glob(os.path.join(fastq_dir, f"*.gz"), recursive=False))
-    files = set(map(lambda x: (x.split("/")[-1].split("_")[0], x), files))
+    files = set(map(lambda x: (x.split("/")[-1].rsplit(".")[0].rsplit("_")[0], x), files)) if mode != "single" else set(map(lambda x: (x.split("/")[-1].rsplit(".")[0], x), files))
 
     for SRR, path in files:
         dictionary[SRR].append(path)
@@ -27,7 +29,14 @@ def main(args=None):
     with open('input.csv', 'w') as f:
         print("Sample,R1,R2", file=f)
         for SRR in dict(dictionary):
-            print(f"{SRR.strip()},{WD + '/' + dict(dictionary)[SRR][0].strip()},{WD + '/' + dict(dictionary)[SRR][1].strip()}", file=f)
+            if len(dictionary[SRR]) < 2:
+                print(f"{SRR.strip()},{WD + '/' + dict(dictionary)[SRR][0].strip()},", file=f)
+
+            else:
+                print(f"{SRR.strip()},{WD + '/' + dict(dictionary)[SRR][0].strip()},{WD + '/' + dict(dictionary)[SRR][1].strip()}", file=f)
+
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- scripts/io.py
+++ scripts/io.py
@@ -11,8 +11,20 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+BANNER = f"""
+{bcolors.HEADER}
+ __  __      _                                         _          
+|  \/  |    | |                                       (_)         
+| \  / | ___| |_ __ _  __ _  ___ _ __   ___  _ __ ___  _  ___ ___ 
+| |\/| |/ _ \ __/ _` |/ _` |/ _ \ '_ \ / _ \| '_ ` _ \| |/ __/ __|
+| |  | |  __/ || (_| | (_| |  __/ | | | (_) | | | | | | | (__\__ \\
+|_|  |_|\___|\__\__,_|\__, |\___|_| |_|\___/|_| |_| |_|_|\___|___/
+                       __/ |                                      
+genes without jeans   |___ /                                       
+{bcolors.ENDC}
+"""
 
-parser = argparse.ArgumentParser(description="This is a Snakemake workflow for the functional analysis of metagenomic WGS read data.", epilog="Example usage: snakemake --config reads=<INPUT_CSV>" )
-parser.add_argument("FASTQ_DIR", help="Description for foo argument")
+parser = argparse.ArgumentParser(description="This is a Snakemake workflow for the functional analysis of metagenomic WGS read data.", epilog="Example usage: snakemake --config reads=<INPUT_CSV>", add_help=False)
+parser.add_argument("--help", help="Description for foo argument")
 args, unknown = parser.parse_known_args()
 
